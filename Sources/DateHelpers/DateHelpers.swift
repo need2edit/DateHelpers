6b66d1fb457449dd26c4564287e5fd3868285a77
Jake Young <need2edit@gmail.com>
2019-07-12 15:21:40 -0400

    Adds days, months, years from another date

--- Sources/DateHelpers/DateHelpers.swift
+++ Sources/DateHelpers/DateHelpers.swift
@@ -1,3 +1,17 @@
-struct DateHelpers {
-    var text = "Hello, World!"
+import Foundation
+
+extension Date {
+    
+    public func days(between other: Date) -> Int {
+        return Calendar.current.dateComponents([.day], from: self, to: other).day ?? 0
+    }
+    
+    public func months(between other: Date) -> Int {
+        return Calendar.current.dateComponents([.month], from: self, to: other).month ?? 0
+    }
+    
+    public func years(between other: Date) -> Int {
+        return Calendar.current.dateComponents([.year], from: self, to: other).year ?? 0
+    }
+    
 }
--- Tests/DateHelpersTests/DateHelpersTests.swift
+++ Tests/DateHelpersTests/DateHelpersTests.swift
@@ -2,14 +2,33 @@ import XCTest
 @testable import DateHelpers
 
 final class DateHelpersTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(DateHelpers().text, "Hello, World!")
+    
+    func test_Interval_Between() {
+        
+        let startString = "01-01-2019"
+        let endString = "02-05-2019"
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM-dd-yyyy"
+        
+        let start = formatter.date(from: startString)!
+        let end = formatter.date(from: endString)!
+        
+        let daysBetween = start.days(between: end)
+        XCTAssertEqual(daysBetween, 35)
+        
+        let daysBetweenReversed = end.days(between: start)
+        XCTAssertEqual(daysBetweenReversed, -35)
+        
+        let monthsBetween = start.months(between: end)
+        XCTAssertEqual(monthsBetween, 1)
+        
+        let monthsBetweenReversed = end.months(between: start)
+        XCTAssertEqual(monthsBetweenReversed, -1)
+        
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("test_Days_Between", test_Interval_Between),
     ]
 }
